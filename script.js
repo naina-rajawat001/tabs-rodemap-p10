d6b2d6b325fb8a66e34a47cb11f0468b711dfe32
naina-rajawat001 <naina.rajawat001@gmail.com>
2026-04-26 09:06:31 +0530

    commit

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const tabs = document.querySelectorAll(".tabs")
+const contentBox = document.querySelector("p")
+let tabContent = "First Tab"
+
+const defaultTab = document.getElementById("tab1")
+defaultTab.classList.add("active-tab")
+
 
 for(let tab of tabs) {
     tab.addEventListener("click", (event)=> {
-        tab.classList.remove("active-tab")
-        console.log(event)
-        el.classList.add("active-tab")
+        tabs.forEach(t => t.classList.remove("active-tab"))
+        const activeTab = event.target
+        const tabname = event.target.id
+        if(tabname == "tab1") {
+            tabContent = "First Tab"
+        }else if(tabname == "tab2") {
+            tabContent = "Second Tab"
+        }else if (tabname == "tab3"){
+            tabContent = "Third Tab"
+        }else if (tabname == "tab4") {
+            tabContent = "Fourth Tab"
+        }
+        activeTab.classList.add("active-tab")
+        contentBox.innerText = `${tabContent} content to be displayed here`
     })
+
 }
 
 
